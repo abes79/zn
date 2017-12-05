096d5ca790082690f7edd692c84dc076bb412095
abes79 <abes1979@gmail.com>
2017-12-06 00:13:14 +0100

    działający loginUser

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -14,33 +14,25 @@ export class LoginComponent {
   userLogin: string = 'admin';
   userPassword: string = 'admin';
   dataArray: any = [];
-  isLogin: boolean = false;
 
   constructor(private router: Router, private service: AppService, private _http: HttpClient) { }
 
   loginUser() {
-    this.loginSql();
-    this.service.setUserName(this.userLogin);
-    if (false) {
-      console.log("isLogin in loginUser method: " + this.isLogin);
-      this.service.setIsUserLogin(true);
-      this.router.navigate(['/']);
-    } else {
-      console.log("isLogin in loginUser method: " + this.isLogin);
-      this.service.setIsUserLogin(false);
-      this.router.navigate(['/login']);
-    }
-  }
-
-  loginSql() {
     let toPost: string = '{ "sqlRequest" : "22", "sqlQuery" : "SELECT * FROM login WHERE login=\'' + this.userLogin +'\'", "sqlPassword" : "'+ this.userPassword +'" }';
     let jsonPost: JSON = JSON.parse(toPost);
     let _url: string = 'http://abes79.linuxpl.info/zn/db_sql.php';
-    this._http.post(_url, jsonPost
+    return this._http.post(_url, jsonPost
     ).subscribe((data) => {
       this.dataArray = data;
-      console.log("In loginSQL method " + this.dataArray[0].isLogin);
-      this.isLogin = this.dataArray[0].isLogin;
+      //console.log("loginSQL method: " + this.dataArray[0].isLogin);
+      if (this.dataArray[0].isLogin == 'true') {
+        this.service.setUserName(this.userLogin);
+        this.service.setIsUserLogin(true);
+        this.router.navigate(['/']);
+      } else {
+        this.service.setIsUserLogin(false);
+        this.router.navigate(['/login']);
+      }
     })
   }
 }
